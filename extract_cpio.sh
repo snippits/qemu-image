0209ce884573814b9546518d187771eb4060e976
Medicine Yeh <medicinehy@gmail.com>
2017-01-26 12:24:33 +0800

    Remove checks in extract_cpio.sh

--- extract_cpio.sh
+++ extract_cpio.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
 
-if [ -d ./rootfs ] ; then
-    if [ -e ./rootfs.cpio ] ; then
-        while true; do
-            read -p "Are you sure you want to overwrite rootfs directory?" yn
-            case $yn in
-                [Yy]* ) break;;
-                [Nn]* ) exit;;
-                * ) echo "Please answer yes or no.";;
-            esac
-        done
-    fi
-else
-    mkdir rootfs
-fi
-
 cd rootfs/
 cp ../rootfs.cpio ./
 sudo cpio -ivdu < rootfs.cpio 
