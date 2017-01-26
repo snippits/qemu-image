316f2d9ba1939a7ce2da049d56a561d843a655e4
Medicine Yeh <medicinehy@gmail.com>
2017-01-26 12:29:49 +0800

    Print message before sudo

--- extract_cpio.sh
+++ extract_cpio.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+mkdir -p rootfs
 cd rootfs/
 cp ../rootfs.cpio ./
-sudo cpio -ivdu < rootfs.cpio 
+echo "Extracting cpio image, it requires root privilege to create rootfs"
+sudo cpio -ivdu < rootfs.cpio
 rm rootfs.cpio 
 cd ../
 
