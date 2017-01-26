1f7be84f46b71948d3acaf3a4a90350f73113b13
Medicine Yeh <medicinehy@gmail.com>
2017-01-26 12:22:58 +0800

    Add file check. move install_command.sh to snippits/snippits

--- download.sh
+++ download.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-wget http://pas.csie.ntu.edu.tw/shared/qemu_images.tar
+[[ ! -f qemu_images.tar ]] && wget http://pas.csie.ntu.edu.tw/shared/qemu_images.tar
 tar -xf ./qemu_images.tar
 ./extract_cpio.sh
 
-wget http://pas.csie.ntu.edu.tw/shared/arch_arm.tar
+[[ ! -f arch_arm.tar ]] && wget http://pas.csie.ntu.edu.tw/shared/arch_arm.tar
 tar -xf ./arch_arm.tar
 
