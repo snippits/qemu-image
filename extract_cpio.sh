4606ff2bd4050ef6a250b10cf68b6fd15a25aa97
Medicine Yeh <medicinehy@gmail.com>
2017-01-26 11:23:35 +0800

    Update scripts

--- extract_cpio.sh
+++ extract_cpio.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 if [ -d ./rootfs ] ; then
-	if [ -e ./rootfs.cpio ] ; then
-	while true; do
-		read -p "Are you sure you want to overwrite rootfs directory?" yn
-		case $yn in
-			[Yy]* ) break;;
-			[Nn]* ) exit;;
-			* ) echo "Please answer yes or no.";;
-		esac
-	done
-	fi
+    if [ -e ./rootfs.cpio ] ; then
+        while true; do
+            read -p "Are you sure you want to overwrite rootfs directory?" yn
+            case $yn in
+                [Yy]* ) break;;
+                [Nn]* ) exit;;
+                * ) echo "Please answer yes or no.";;
+            esac
+        done
+    fi
 else
-	mkdir rootfs
+    mkdir rootfs
 fi
 
 cd rootfs/
